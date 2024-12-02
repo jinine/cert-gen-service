ba65c0a7dde282ae3d4b244ae8b9e65e054f8cd6
Tristan Engen <jinine@gmail.com>
2024-12-02 12:19:28 -0800

    complete cert generation without the use of a template

--- src/utils/generateCert.js
+++ src/utils/generateCert.js
@@ -1,11 +1,13 @@
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
-function generateCertificate(data, outputPath) {
+export default function generateCertificate(data, outputPath) {
   const doc = new PDFDocument();
   doc.pipe(fs.createWriteStream(outputPath));
 
-  // Add certificate content
+  const uniqueId = uuidv4();
+
   doc.fontSize(25).text('Certificate of Completion', { align: 'center' });
   doc.moveDown();
   doc.fontSize(20).text(`This certifies that ${data.name}`, { align: 'center' });
@@ -14,7 +16,17 @@ function generateCertificate(data, outputPath) {
   doc.moveDown();
   doc.fontSize(15).text(`Date: ${data.date}`, { align: 'center' });
 
-  doc.end();
-}
+  if (data.signaturePath) {
+    doc.image(data.signaturePath, doc.page.width / 2 - 50, doc.y + 20, { width: 100 });
+  }
+  if (data.stampPath) {
+    doc.image(data.stampPath, doc.page.width - 150, doc.y - 50, { width: 100 });
+  }
 
-export default generateCertificate;
+  doc.moveDown(3);
+  doc.fontSize(10).text(`Certificate ID: ${uniqueId}`, { align: 'center' });
+
+  doc.end();
+
+  return uniqueId; // Return the unique ID for reference
+};
