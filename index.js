b7eab192d83595a0b11ab148447aaeb4eb551bde
Tristan Engen <jinine@gmail.com>
2024-12-02 12:44:13 -0800

    adding input validations

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import generateCertificate from './lib/generateCert.js';
 
 //Example Usage: 
 
-// const certificateData = {
-//     name: 'John Doe',
-//     course: 'Introduction to JavaScript',
-//     date: '2024-12-01',
-//     signaturePath: './public/signatures/signature.png',
-//     stampPath: './public/stamps/example.png'
-//   };
+const certificateData = {
+    name: 'John Doe',
+    course: 'Introduction to JavaScript',
+    date: '2024-12-01',
+    signaturePath: './public/signatures/signature.png',
+    stampPath: './public/stamps/example.png'
+  };
   
-//   const outputPath = './certifications/certificate.pdf';
+  const outputPath = './certifications/certificate.pdf';
   
-//   const certificateId = generateCertificate(certificateData, outputPath);
-//   console.log(`Certificate generated with ID: ${certificateId}`);
+  const certificateId = generateCertificate(certificateData, outputPath);
+  console.log(`Certificate generated with ID: ${certificateId}`);
 
 export { generateCertificate };
